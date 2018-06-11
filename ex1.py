ccc1fb13ce3183376084d20e7623c680b242212f
Marlar <marlargiri54@gmail.com>
2018-06-11 23:33:20 +0630

    ex7.py

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+Print "Hello Again"
+print "I like typing this."
+print "This is fun"
+print "Yay! Printing."
+print "I'd much rather ou 'not'."
+print 'I "said" do not touch this.'
---
+++ ex5.py
@@ -0,0 +1,7 @@
+print "Let's tallk about %5." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %5 eyes and %5 hair." % (my_eyes, my_hair)
+print "His teeth are usually %5 depending on the coffee." % my_teeth
+
